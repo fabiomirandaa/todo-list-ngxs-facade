eb9afbf4f30b7aaba9df127f6f6f07b00e05ef6e
Fábio Miranda <fabioviannatj@gmail.com>
2019-12-25 12:33:12 -0300

    fix: nanoid import

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from './models/Todo';
 import { environment } from 'src/environments/environment';
-import { nanoid } from 'nanoid';
+import * as nanoid from 'nanoid';
 
 @Injectable({
   providedIn: 'root'
