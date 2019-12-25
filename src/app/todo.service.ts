59c14b38faa385b74536a32b7328445caaab0f96
Fábio Miranda <fabioviannatj@gmail.com>
2019-12-25 12:22:18 -0300

    refactor(todo-service): add url environment and nanoid

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from './models/Todo';
+import { environment } from 'src/environments/environment';
+import { nanoid } from 'nanoid';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class TodoService {
+  private _baseUrl: string;
 
   constructor(private http: HttpClient) {
+    this._baseUrl = environment.API_URL;
   }
 
   fetchTodos() {
-    return this.http.get<Todo[]>('http://localhost:3000/todos');
+    return this.http.get<Todo[]>(`${this._baseUrl}/todos`);
   }
 
   deleteTodo(id: number) {
-    return this.http.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    return this.http.delete(`${this._baseUrl}/${id}`);
   }
 
   addTodo(payload: Todo) {
-    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', payload);
+    payload.id = nanoid(12);
+    return this.http.post<Todo>(`${this._baseUrl}/todos`, payload);
   }
 
   updateTodo(payload: Todo, id: number) {
-    return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, payload);
+    return this.http.put<Todo>(`${this._baseUrl}/todos/${id}`, payload);
   }
 }
