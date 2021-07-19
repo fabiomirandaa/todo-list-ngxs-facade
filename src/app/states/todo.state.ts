b1158ae4978385a11437d6967105a7d3df9a1f24
Fábio Miranda <fabiomirandadev@gmail.com>
2021-07-19 18:31:21 -0300

    chore: update Angular to v9

--- src/app/states/todo.state.ts
+++ src/app/states/todo.state.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@angular/core';
 import {
   State,
   Action,
@@ -21,6 +22,7 @@ export class TodoStateModel {
     selectedTodo: null
   }
 })
+@Injectable()
 export class TodoState {
 
   constructor(private todoService: TodoService) {
